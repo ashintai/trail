1b5d3013c523dbb11f2ad2b224ee71f77c06a91a
ashintai <156777132+ashintai@users.noreply.github.com>
2024-06-26 12:03:58 +0900

    ReReRe Http->Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //httpをhttpsへ強制　移行
-        if (\App::enviroment(['production'])){
+        if (\App::environment(['production'])){
             \URL::forceScheme('https');
         }
     }
