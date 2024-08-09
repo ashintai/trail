0513adf99be16f10f81e17954af146e78f9725f2
ashintai <156777132+ashintai@users.noreply.github.com>
2024-08-09 11:30:51 +0900

    PDF02

--- resources/views/dompdf/pdf.blade.php
+++ resources/views/dompdf/pdf.blade.php
@@ -5,22 +5,22 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <title>駐車券PDF出力</title>
-        <style>
+        <style type="text/css" >
         /* dompdf日本語文字化け対策 */
             @font-face {
-                font-family: 'ipag';
+                font-family: ipag;
                 font-style: normal;
                 font-weight: normal;
                 src: url('{{ storage_path('fonts/ipag.ttf') }}') format('truetype');
             }
             @font-face {
-                font-family: 'ipag';
+                font-family: ipag;
                 font-style: bold;
                 font-weight: bold;
                 src: url('{{ storage_path('fonts/ipag.ttf') }}') format('truetype');
             }
             body {
-                font-family: 'ipag' , sans-serif;
+                font-family: ipag , sans-serif;
             }   
         </style>
 
